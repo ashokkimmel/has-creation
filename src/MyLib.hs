f8f5333eb4f461aedf9f2705258f3d90df2bb3a4
ashok <ashok.kimmel@gmail.com>
2025-11-02 15:59:13 -0600

    Working example!

--- src/MyLib.hs
+++
@@ -1,6 +0,0 @@
-{-# LANGUAGE TemplateHaskell #-}
-module MyLib where
---import Language.Haskell.TH
-import Control.Has.Error (hasFrom)
-
-$(hasFrom ''Bool)
--- src/MyLib2.hs
+++
@@ -1,8 +0,0 @@
-{-# LANGUAGE TemplateHaskell #-}
-module MyLib2 where
-import MyLib 
---import Language.Haskell.TH
-data BoolSum a = BoolS Bool | Id a deriving Show
-$(generateCatchBool ''BoolSum)
-
---[| instance Show (a -> b) where show _ = "<function>" |]
